3582e5c208ce1606ddc307e388accc8cc4968d80
ReverseStateMonad <electroniccircus@yahoo.com>
2016-10-28 23:14:27 -0400

    Factory factory
    Automated generation of building entities.

--- prototypes/entity/factory.lua
+++ prototypes/entity/factory.lua
@@ -1,29 +1,75 @@
 require ("prototypes.copied-from-base.demo-pipecovers")
 require ("prototypes.copied-from-base.circuit-connector-sprites")
 
-data:extend({
-	-- FACTORY --
-	{
+-- Defines
+local SIZE_SMALL = 3
+local SIZE_MEDIUM = 6
+local SIZE_LARGE = 9
+local SIZE_HUGE = 12
+
+local function index_size(size)
+	if size == SIZE_SMALL then
+		return "small"
+	elseif size == SIZE_MEDIUM then
+		return "medium"
+	elseif size == SIZE_LARGE then
+		return "large"
+	elseif size == SIZE_HUGE then
+		return "huge"
+	end
+	return nil -- should never happen
+end
+
+-- Helper function
+local function get_connection_points(size)
+	local result = {}
+	local connection_radius = math.floor(size * 2 / 3)
+	local c1 = size + 0.5
+	for c2 = 0.5 - connection_radius, connection_radius - 0.5 do
+		table.insert(result, { position = { -c1, c2 }, type = "output" })
+		table.insert(result, { position = { c2, -c1 }, type = "output" })
+		table.insert(result, { position = { c1, c2 }, type = "output" })
+	end
+	return result
+end
+
+local function create_building(size, category)
+	local name = index_size(size) .. "-" .. category
+	local icon = "__Factorissimo__/graphics/icons/" .. name .. ".png"
+	local input, output, priority, usage
+	if category == "factory" then
+		input = factorissimo.config.power_input_limit
+		output = "0MW"
+		priority = "secondary-input"
+		usage = input
+	elseif category == "power-plant" then
+		input = "0MW"
+		output = factorissimo.config.power_output_limit
+		priority = "secondary-output"
+		usage = output
+	end
+	local image_prefix = "__Factorissimo__/graphics/entity/" .. name .. "/" .. name
+	return {
 		type = "assembling-machine",
-		name = "small-factory",
-		icon = "__Factorissimo__/graphics/icons/small-factory.png",
+		name = name,
+		icon = icon,
 		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 10, result = "small-factory"},
-		max_health = 5000,
+		minable = {hardness = 0.2, mining_time = 10, result = name},
+		max_health = 1000 * (size + 2),
 		corpse = "big-remnants",
-		collision_box = {{-2.95, -2.95}, {2.95, 2.4}},
-		selection_box = {{-3, -3}, {3, 3}},
+		collision_box = {{.05 - size, .05 - size}, {size - .05, size - .6}},
+		selection_box = {{-size, -size}, {size, size}},
 		dying_explosion = "medium-explosion",
 		energy_source =
 		{
 			type = "electric",
-			usage_priority = "secondary-input",
-			input_flow_limit = factorissimo.config.power_input_limit,
-			output_flow_limit = "0MW",
+			usage_priority = priority,
+			input_flow_limit = input,
+			output_flow_limit = output,
 			buffer_capacity = factorissimo.config.power_buffer,
 			drain = "0W"
 		},
-		energy_usage = factorissimo.config.power_input_limit,
+		energy_usage = usage,
 		
 		pumping_speed = 0,
 		fluid_boxes =
@@ -33,21 +79,7 @@ data:extend({
 				base_area = 1,
 				base_level = 1,
 				pipe_covers = pipecoverspictures(),
-				pipe_connections =
-				{
-					{ position = {-3.5, -1.5}, type="output" },
-					{ position = {-3.5, -0.5}, type="output" },
-					{ position = {-3.5, 0.5}, type="output" },
-					{ position = {-3.5, 1.5}, type="output" },
-					{ position = {-1.5, -3.5}, type="output" },
-					{ position = {-0.5, -3.5}, type="output" },
-					{ position = {0.5, -3.5}, type="output" },
-					{ position = {1.5, -3.5}, type="output" },
-					{ position = {3.5, -1.5}, type="output" },
-					{ position = {3.5, -0.5}, type="output" },
-					{ position = {3.5, 0.5}, type="output" },
-					{ position = {3.5, 1.5}, type="output" },
-				},
+				pipe_connections = get_connection_points(size),
 				off_when_no_fluid_recipe = false
 			}
 		},
@@ -55,7 +87,7 @@ data:extend({
 		{
 		  north =
 		  {
-			filename = "__Factorissimo__/graphics/entity/small-factory/small-factory-north.png",
+			filename = image_prefix .. "-north.png",
 			width = 288,
 			height = 256,
 			frame_count = 1,
@@ -63,7 +95,7 @@ data:extend({
 		  },
 		  east =
 		  {
-			filename = "__Factorissimo__/graphics/entity/small-factory/small-factory-east.png",
+			filename = image_prefix .. "-east.png",
 			width = 288,
 			height = 256,
 			frame_count = 1,
@@ -71,7 +103,7 @@ data:extend({
 		  },
 		  south =
 		  {
-			filename = "__Factorissimo__/graphics/entity/small-factory/small-factory-south.png",
+			filename = image_prefix .. "-south.png",
 			width = 288,
 			height = 256,
 			frame_count = 1,
@@ -79,110 +111,7 @@ data:extend({
 		  },
 		  west =
 		  {
-			filename = "__Factorissimo__/graphics/entity/small-factory/small-factory-west.png",
-			width = 288,
-			height = 256,
-			frame_count = 1,
-			shift = {0.5, 0.25}
-		  }
-		},
-		vehicle_impact_sound =	{ filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		working_sound =
-		{
-			sound = { filename = "__base__/sound/roboport-working.ogg", volume = 0.0 },
-			max_sounds_per_type = 3,
-			audible_distance_modifier = 0.5,
-			probability = 0 / (5 * 60)
-		},
-		
-		-- unused assembling-machine features
-		module_specification =
-		{
-		  module_slots = 0
-		},
-		crafting_categories = { "factorissimo-no-recipes" },
-		crafting_speed = 1,
-		ingredient_count = 0
-	},
-	
-	-- POWER PLANT --
-	{
-		type = "assembling-machine",
-		name = "small-power-plant",
-		icon = "__Factorissimo__/graphics/icons/small-power-plant.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 10, result = "small-power-plant"},
-		max_health = 5000,
-		corpse = "big-remnants",
-		collision_box = {{-2.95, -2.95}, {2.95, 2.4}},
-		selection_box = {{-3, -3}, {3, 3}},
-		dying_explosion = "medium-explosion",
-		energy_source =
-		{
-			type = "electric",
-			usage_priority = "secondary-output",
-			input_flow_limit = "0MW",
-			output_flow_limit = factorissimo.config.power_output_limit,
-			buffer_capacity = factorissimo.config.power_buffer,
-			drain = "0W"
-		},
-		energy_usage = factorissimo.config.power_input_limit,
-		
-		pumping_speed = 0,
-		fluid_boxes =
-		{
-			{
-				production_type = "output",
-				base_area = 1,
-				base_level = 1,
-				pipe_covers = pipecoverspictures(),
-				pipe_connections =
-				{
-					{ position = {-3.5, -1.5}, type="output" },
-					{ position = {-3.5, -0.5}, type="output" },
-					{ position = {-3.5, 0.5}, type="output" },
-					{ position = {-3.5, 1.5}, type="output" },
-					{ position = {-1.5, -3.5}, type="output" },
-					{ position = {-0.5, -3.5}, type="output" },
-					{ position = {0.5, -3.5}, type="output" },
-					{ position = {1.5, -3.5}, type="output" },
-					{ position = {3.5, -1.5}, type="output" },
-					{ position = {3.5, -0.5}, type="output" },
-					{ position = {3.5, 0.5}, type="output" },
-					{ position = {3.5, 1.5}, type="output" },
-				},
-				off_when_no_fluid_recipe = false
-			}
-		},
-		animation =
-		{
-		  north =
-		  {
-			filename = "__Factorissimo__/graphics/entity/small-power-plant/small-power-plant-north.png",
-			width = 288,
-			height = 256,
-			frame_count = 1,
-			shift = {0.5, 0.25}
-		  },
-		  east =
-		  {
-			filename = "__Factorissimo__/graphics/entity/small-power-plant/small-power-plant-east.png",
-			width = 288,
-			height = 256,
-			frame_count = 1,
-			shift = {0.5, 0.25}
-		  },
-		  south =
-		  {
-			filename = "__Factorissimo__/graphics/entity/small-power-plant/small-power-plant-south.png",
-			width = 288,
-			height = 256,
-			frame_count = 1,
-			shift = {0.5, 0.25}
-		  },
-		  west =
-		  {
-			filename = "__Factorissimo__/graphics/entity/small-power-plant/small-power-plant-west.png",
+			filename = image_prefix .. "-west.png",
 			width = 288,
 			height = 256,
 			frame_count = 1,
@@ -207,4 +136,12 @@ data:extend({
 		crafting_speed = 1,
 		ingredient_count = 0
 	}
+end
+
+data:extend({
+	-- FACTORY --
+	create_building(SIZE_SMALL, "factory"),
+	
+	-- POWER PLANT --
+	create_building(SIZE_SMALL, "power-plant")
 })
